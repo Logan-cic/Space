8069e3b565e17bad32dc322837180a3175ec43cd
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-01-17 21:23:14 -0300

    add files front load static files and add url view

--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gallery/index.html')
+
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
